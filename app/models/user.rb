3987a544fadd6257f1dc2abba2d6dca47e0ee0aa
Boris Skok <skokcmd@gmail.com>
2023-10-07 16:51:29 +0200

    restricted deleting user with associated tasks

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   mount_uploader  :avatar, AvatarUploader
-
+  has_many :tasks, dependent: :restrict_with_error
   def full_name
     "#{first_name} #{last_name}"
   end
